b6883e90960ee0480f3153c70167bb038ec73672
pritykumari04012005-glitch <pritykumari04012005@gmail.com>
2025-09-07 20:24:47 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,18 @@
+public class Arithmatic {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a%b);
+        double x = 5;
+        double y = 2;
+        System.out.println(x+y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+    }
+}
+
---
+++ OrderofOperator.java
@@ -0,0 +1,7 @@
+public class OrderofOperator {
+    public static void main(String[] args) {
+        System.out.println(8+8*5);
+        System.out.println(9/3/3);
+        System.out.println(6/(3/3+2));
+    }
+}
